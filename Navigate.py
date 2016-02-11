e25baed68757c4c9a5c97d9ac0fd871936bf749e
Lt1201 <ltennant1201@gmail.com>
2016-02-11 18:16:59 -0500

    Support for BET 1 video ads
    Reward Rack is stepping up their game...

--- Navigate.py
+++ Navigate.py
@@ -21,14 +21,17 @@ s1Buttons = "Earn3.00MorePoints"
 s2Buttons = "Earn6.00MorePoints"
 s3Buttons = "Earn4.00MorePoints"
 s4Buttons = "Earn2.00MorePoints"
+s6Buttons = "Earn0.50MorePoints"
 claimButtons = "ClaimPoints"
 s1 = "DISCOVERUNIVISION"
 s2 = "DISCOVERBET"
 s3 = "discoversmallbet"
 s4 = "DISCOVERMASHABLE"
 s5 = "DISCOVERRRDISCOVERY"
+s6 = "DISCOVERBET1VIDEO"
 titleCompare1 = "REWARDRACK|OFFER:MASHABLE"
 titleCompare5 = "REWARDRACK|OFFER:RRDISCOVERY"
+titleCompare6 = "REWARDRACK|OFFER:BET1VIDEO"
 failCount = 0
 title = ""
 header1 = ""
@@ -473,7 +476,85 @@ def RR_Discovery():
         browser.find_element_by_xpath("//a").click()
 
 
+def OneVideo():
+    global s1Buttons
+    global s2Buttons
+    global s3Buttons
+    global s1
+    global s2
+    global s3
+    global titleCompare1
+    global title
+    global header1
+    global parentWindowHandle
+    global offerWindowHandle
+    global memecount
+    global failCount
+    global browser
+    global refresh
+    global iterations
 
+
+    waiter = WebDriverWait(browser, 210)
+    try:
+        element = waiter.until(EC.element_to_be_clickable((By.ID,"btn-next-step")))
+        print "Step one done waiting"
+        browser.find_element_by_link_text("Next Step").click()
+        element = waiter.until(EC.element_to_be_clickable((By.XPATH,"//a")))
+        print "Claim waiting"
+        browser.find_element_by_xpath("//a").click()
+    except BaseException:
+        failCount += 1
+        print "BROKEN COUNTER: " + str(failCount)
+        if failCount <= 3:
+            browser.quit()
+            Main(memecount)
+
+
+    time.sleep(2)
+    memecount += 1
+    buttonCheck = browser.find_element_by_xpath("//a").text
+    buttonCheck = buttonCheck.replace(" ","")
+    #print buttonCheck.lower()
+        
+
+    if buttonCheck.lower() == s1Buttons.lower():
+        print "next offer button clicked"
+        browser.find_element_by_link_text("Earn 3.00 More Points").click()
+        header1 = "discoverunivision"
+        print "Spanish has continued"
+        offerWindowHandle = None
+
+    elif buttonCheck.lower() == s2Buttons.lower():
+        print "next offer button clicked"
+        browser.find_element_by_link_text("Earn 6.00 More Points").click()
+        header1 = "discoverbet"
+        offerWindowHandle = None
+        print "Black has continued"
+
+    elif buttonCheck.lower() == s3Buttons.lower():
+        header1 = "discoversmallbet"
+        print "Small bet has continued"
+        offerWindowHandle = None
+        browser.find_element_by_link_text("Earn 4.00 More Points").click()
+
+    elif buttonCheck.lower() == s4Buttons.lower():
+        header1 = "DISCOVERMASHABLE"
+        print "mashy has continued"
+        OfferWindowHandle = None
+        browser.find_element_by_link_text("Earn 2.00 More Points").click()
+
+
+    elif buttonCheck.lower() == s6Buttons.lower():
+        header1 = "DISCOVERBET1VIDEO"
+        print "1 video bet has continued"
+        browser.find_element_by_link_text("Earn 0.50 More Points").click()
+
+    else:
+        print "Dank has found something new" + buttonCheck.lower()
+        browser.find_element_by_xpath("//a").click()
+
+    
 
 
 def Main(x):
@@ -594,6 +675,16 @@ def Main(x):
             titleCheck()
 
             Mashables()
+
+
+
+        elif header1.lower() == s6.lower():
+
+            titleCheck()
+
+            OneVideo()
+
+
 
         else:
             print "dnak memes are broken"
